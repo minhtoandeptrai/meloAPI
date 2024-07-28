e62909fc93e85938556eb29dc7043f89e6770139
minhtoandeptrai <101089170+minhtoandeptrai@users.noreply.github.com>
2024-07-28 17:22:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
-import controllers.song_controller
+
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
     return 'MeloSpace API'
+
+import controllers.song_controller
