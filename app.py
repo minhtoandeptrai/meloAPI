fb4d50634eaa2c449273ca1527cda7be1020c7c6
minhtoandeptrai <lebi.signer03@gmail.com>
2024-07-29 07:36:26 +0700

    aaa

--- app.py
+++ app.py
@@ -7,8 +7,4 @@ CORS(app)
 @app.route('/')
 def welcome():
     return 'MeloSpace API'
-<<<<<<< HEAD
-# import controllers.song_controller
-=======
-import controllers.song_controller
->>>>>>> 8042adac7783d6ffb0a966516ddb9c4329a0c4e2
+# importcontrollers.song_controller
